13b132bee3582aa027a389d889b6786e58e8d167
ahme abobakr <ah.abobakr88@gmail.com>
2020-03-18 08:15:04 +0200

    adding save menu item

--- app/src/main/java/com/example/weatherapp/home/mvvm/views/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/weatherapp/home/mvvm/views/fragments/HomeFragment.kt
@@ -1,5 +1,9 @@
 package com.example.weatherapp.home.mvvm.views.fragments
 
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.databinding.ViewDataBinding
 import com.example.weatherapp.R
 import com.example.weatherapp.base.mvvm.views.BaseFragment
@@ -8,5 +12,22 @@ class HomeFragment<FragmentHomeBinding : ViewDataBinding>: BaseFragment<Fragment
 
     override fun getLayoutID(): Int {
         return R.layout.fragment_home
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_save -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+
     }
 }
