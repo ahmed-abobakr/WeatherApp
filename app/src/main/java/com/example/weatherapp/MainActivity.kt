fb5e32d2efe506aa6d982ef593fe1a4a63b06a05
Ahmed.Abubakr <ah.abobakr88@gmail.com>
2023-06-07 09:38:46 +0300

    add appcenter connection

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //connect to AppCenter
+        AppCenter.start(application, "7516a0ef-f101-4409-81cf-65f31c94d950", Analytics::class.java, Crashes::class.java)
     }
 }
